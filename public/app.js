992f33732f5089ff8f8d0349b58b5be5282f46f9
BanafshaHotak <azmeenashah@yahoo.com>
2020-08-20 23:37:47 +0500

    made changes with database

--- public/app.js
+++ public/app.js
@@ -1,11 +1,9 @@
 var list = document.getElementById("li_ul");
+firebase.database().ref('todos').on('child_added',function (data){
 
-function addTodo(){
-var todo_item = document.getElementById("todo-item")
- 
 // creat li and add text
 var li = document.createElement('li')
-var liText = document.createTextNode(todo_item.value)
+var liText = document.createTextNode(data.val().value)
 li.setAttribute("class","txt")
 li.appendChild(liText)
 
@@ -14,6 +12,7 @@ var delButn = document.createElement("button")
 var delText = document.createTextNode("Delete")
 delButn.appendChild(delText)
 delButn.setAttribute("class","btn")
+delButn.setAttribute('id',data.val().key)
 delButn.setAttribute("onclick","deleteItem(this)")
 li.appendChild(delButn)
 
@@ -22,23 +21,47 @@ var EdtBtn = document.createElement("button");
 var EdtText = document.createTextNode("Edit")
 EdtBtn.appendChild(EdtText)
 EdtBtn.setAttribute("class","btn1")
+EdtBtn.setAttribute('id',data.val().key)
 li.appendChild(EdtBtn)
 EdtBtn.setAttribute("onclick","editItem(this)")
 
 
 list.appendChild(li)
+  
+})
+
+function addTodo() {
+var todo_item = document.getElementById("todo_item");
+  console.log(todo_item.value)
+  var key = firebase.database().ref('todos').push().key
+
+var todo ={
+value: todo_item.value,
+key: key  
+}
+firebase.database().ref('todos').child(key).set(todo)
+
 todo_item.value = ""
 
 }
 
 function deleteItem(e){
+
+firebase.database().ref('todos').child(e.id).remove()
 e.parentNode.remove()
 }
 
 
 function editItem(e) {
   var val = prompt("Enter updated value",e.parentNode.firstChild.nodeValue)
-  e.parentNode.firstChild.nodeValue = val;
+  var editTodo = {
+  value : val,
+  key : e.id
+  
+}
+firebase.database().ref('todos').child(e.id).set(editTodo)
+e.parentNode.firstChild.nodeValue = val;
+
 }
 
 
